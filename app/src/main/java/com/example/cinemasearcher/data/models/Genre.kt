dc3a85dd7202c6da2c256fe208f6fcde64c2b6a6
Artem Tikhonov <temaTihonov@yandex.ru>
2022-08-23 16:45:15 +0300

    Added models

---
+++ app/src/main/java/com/example/cinemasearcher/data/models/Genre.kt
@@ -0,0 +1,6 @@
+package com.example.cinemasearcher.data.models
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/cinemasearcher/data/models/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.example.cinemasearcher.data.models
+
+data class ProductionCompany(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
