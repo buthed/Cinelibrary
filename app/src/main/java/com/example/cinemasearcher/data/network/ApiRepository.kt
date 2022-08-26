f4ca3e3f64ea0ff43d84c1a8b117ebcc70abca87
Artem Tikhonov <temaTihonov@yandex.ru>
2022-08-26 14:25:49 +0300

    With works api

--- app/src/main/java/com/example/cinemasearcher/data/network/ApiRepository.kt
+++ app/src/main/java/com/example/cinemasearcher/data/network/ApiRepository.kt
@@ -1,11 +1,12 @@
 package com.example.cinemasearcher.data.network
 
-import com.example.cinemasearcher.data.models.TMDB_API_KEY
 import javax.inject.Inject
 
 class ApiRepository @Inject constructor(private val apiService: ApiService) {
 
+    suspend fun getMovie() = apiService.getMovie()
+
     suspend fun getPopularMovies() = apiService.getPopularMovies()
 
-    suspend fun getUpcomingMovies() = apiService.getUpcomingMovies()
+//    suspend fun getUpcomingMovies() = apiService.getUpcomingMovies()
 }
--- app/src/main/java/com/example/cinemasearcher/data/network/ApiService.kt
+++ app/src/main/java/com/example/cinemasearcher/data/network/ApiService.kt
@@ -1,18 +1,21 @@
 package com.example.cinemasearcher.data.network
 
-import com.example.cinemasearcher.data.models.Movie
-import com.example.cinemasearcher.data.models.TMDB_API_KEY
-import com.example.cinemasearcher.data.models.TMDB_API_POPULAR
-import com.example.cinemasearcher.data.models.TMDB_API_UPCOMING
+import com.example.cinemasearcher.data.model.ApiConstants.TMDB_API_KEY
+import com.example.cinemasearcher.data.model.ApiConstants.TMDB_API_POPULAR
+import com.example.cinemasearcher.data.model.entites.Movie
+import com.example.cinemasearcher.data.model.entites.PopularMoviesResult
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET(TMDB_API_POPULAR+TMDB_API_KEY)
-    suspend fun getPopularMovies() : Response<List<Movie>>
+    @GET("/3/movie/550?api_key=1cdc07942d44ead0f1079d449b6760a3")
+    suspend fun getMovie(): Response<Movie>
 
-    @GET(TMDB_API_UPCOMING+ TMDB_API_KEY)
-    suspend fun getUpcomingMovies() : Response<List<Movie>>
+
+    @GET(TMDB_API_POPULAR+TMDB_API_KEY)
+    suspend fun getPopularMovies(): PopularMoviesResult
+
+//    @GET(TMDB_API_UPCOMING+ TMDB_API_KEY)
+//    suspend fun getUpcomingMovies() : Response<List<Movie>>
 }
