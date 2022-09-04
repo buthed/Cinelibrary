9975a3b68264d9398a05c6b41893e474dce3103e
Artem Tikhonov <temaTihonov@yandex.ru>
2022-09-04 15:26:16 +0300

    With search category chooser

--- app/src/main/java/com/example/cinemasearcher/presentation/components/search/SearchCategory.kt
+++ app/src/main/java/com/example/cinemasearcher/presentation/components/search/SearchCategory.kt
@@ -39,9 +39,7 @@ fun SearchCategory(text: String, chosenCategory: String, onClick: () -> Unit) {
             )
         }
     } else {
-        Card(Modifier.defaultMinSize(minWidth = 100.dp).clickable {
-                onClick
-        },
+        Card(Modifier.defaultMinSize(minWidth = 100.dp).clickable(onClick = onClick),
             backgroundColor = LocalCLBExtraColors.current.Soft,
             shape = RoundedCornerShape(8.dp)
         ) {
