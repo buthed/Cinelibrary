c8db0b843d4519527aa0c02d01542c8e7ca43f61
Artem Tikhonov <temaTihonov@yandex.ru>
2022-09-08 12:40:18 +0300

    With firebase dependencies

--- app/src/main/java/com/tematikhonov/cinelibrary/presentation/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/tematikhonov/cinelibrary/presentation/ui/profile/ProfileScreen.kt
@@ -2,11 +2,9 @@ package com.tematikhonov.cinelibrary.presentation.ui.profile
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.tematikhonov.cinelibrary.presentation.components.movieDetails.YouTubeTrailerPlayer
 
 @Composable
 fun ProfileScreen() {
-    YouTubeTrailerPlayer("S0Q4gqBUs7c")
 }
 
 @Preview
